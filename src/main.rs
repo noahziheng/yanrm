1c5f46fff457b9790aad00249b538aa26522362f
Noah <noahgaocn@outlook.com>
2023-03-30 00:27:41 +0800

    feat: add basic cli by clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![warn(rust_2018_idioms, clippy::all, clippy::pedantic)]
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    Cli::parse();
 }
